8aea2a2865d1fac1c3bc0924209fb56a9e8f85be
Marian Simko <ariquis@gmail.com>
2013-07-10 18:59:44 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
@@ -6,9 +7,10 @@ var app = express.createServer(express.logger());
 //  response.send('Hello World 2!');
 //});
 
-Var read = fs.readFileSync("index.html");
-Var buff = new Buffer(read);
-response.send(buff.toString(0,buff.lenght)
+var read = fs.readFileSync("index.html");
+var buff = new Buffer(read);
+response.send(buff.toString(0,buff.lenght));
+
 
 Var port = process.env.PORT || 5000;
 app.listen(port, function() {
