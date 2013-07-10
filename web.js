af99de20b19b5771dc7c6e917ffaefc8e31eedb1
Marian Simko <ariquis@gmail.com>
2013-07-10 19:12:51 +0000

    updated web.js

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
-  var read1 = fs.readFileSync("index.html");
+  var read1 = fs.readFileSync("index.html","utf-8");
   var buff = new Buffer(read1);
   response.send(buff.toString());
 });
