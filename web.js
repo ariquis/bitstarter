2abcf3f3462de386ddcec7d9235ecd5cb0ed76d7
Marian Simko <ariquis@gmail.com>
2013-07-10 19:24:42 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
@@ -15,7 +14,7 @@ app.get('/', function(request, response) {
 });
 
 
-Var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
