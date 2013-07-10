62487b15705df8a1ce9f30885d4861e9ff02bf39
Marian Simko <ariquis@gmail.com>
2013-07-10 18:38:38 +0000

    Updated web.js to read from file

--- web.js
+++ web.js
@@ -2,11 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+//app.get('/', function(request, response) {
+//  response.send('Hello World 2!');
+//});
 
-var port = process.env.PORT || 5000;
+Var read = fs.readFileSync("index.html");
+Var buff = new Buffer(read);
+response.send(buff.toString(0,buff.lenght)
+
+Var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
